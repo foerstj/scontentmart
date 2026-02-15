e8c5e9a6c84c728a34f71ae4d2a54a7c70f13d73
Johannes Förstner <johannes@famfoerstner.de>
2026-02-15 21:03:08 +0100

    Add repo scripts

---
+++ build.bat
@@ -0,0 +1,27 @@
+:: map name
+set map=scontentmart
+:: map name, case sensitive
+set map_cs=SContentMart
+
+:: path of Bits dir
+set bits=%~dp0.
+:: path of DS installation
+set ds=%DungeonSiege%
+:: path of TankCreator
+set tc=%TankCreator%
+
+set copyright=CC-BY-SA 2026
+set author=Johannes Förstner
+
+:: Compile map file
+rmdir /S /Q "%tmp%\Bits"
+robocopy "%bits%\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E
+pushd %gaspy%
+venv\Scripts\python -m build.fix_start_positions_required_levels %map% --bits "%tmp%\Bits"
+if %errorlevel% neq 0 pause
+popd
+"%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\Maps\%map_cs%.dsmap" -copyright "%copyright%" -title "%map_cs%" -author "%author%"
+if %errorlevel% neq 0 pause
+
+:: Cleanup
+rmdir /S /Q "%tmp%\Bits"
---
+++ cleanup.bat
@@ -0,0 +1,9 @@
+:: map name, case sensitive
+set map_cs=SContentMart
+:: path of DS installation
+set ds=%DungeonSiege%
+
+:: Cleanup resources so as not to confuse Siege Editor
+del "%ds%\Resources\%map_cs%.dsres"
+:: also map file
+del "%ds%\Maps\%map_cs%.dsmap"
