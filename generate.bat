0fa538f2bc018482be9eb5e74af449cb73e549db
Johannes Förstner <johannes@famfoerstner.de>
2026-02-18 12:25:07 +0100

    Generate shops with GasPy from a csv that I wrote with GasPy

---
+++ generate.bat
@@ -0,0 +1,7 @@
+:: path of Bits dir
+set bits=%~dp0.
+
+pushd "%GasPy%"
+venv\Scripts\python -m jinja gaspy\armor-merchants.gas.jinja world\contentdb\templates\scontentmart\actors --for-all gaspy\armors.csv --bits "%bits%"
+if %errorlevel% neq 0 pause
+popd
