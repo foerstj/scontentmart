484358aa4079dc3f13205fc1300f6a448b8ef06e
Johannes Förstner <johannes@famfoerstner.de>
2026-02-20 15:02:19 +0100

    LoA merchants as well

--- generate.bat
+++ generate.bat
@@ -5,6 +5,8 @@ pushd "%GasPy%"
 venv\Scripts\python -m jinja gaspy\armor-merchants.gas.jinja world\contentdb\templates\scontentmart\actors --for-all gaspy\armors.csv --bits "%bits%"
 if %errorlevel% neq 0 pause
 
-venv\Scripts\python -m jinja gaspy\shops.gas.jinja world\contentdb\templates\scontentmart\actors merchant.gas --for-all gaspy\shops.csv --bits "%bits%"
+venv\Scripts\python -m jinja gaspy\shops.gas.jinja world\contentdb\templates\scontentmart\actors merchant.gas --for-all gaspy\shops.csv --value v=v --bits "%bits%"
+if %errorlevel% neq 0 pause
+venv\Scripts\python -m jinja gaspy\shops.gas.jinja world\contentdb\templates\scontentmart\actors dsx_merchant.gas --for-all gaspy\shops.csv --value v=loa --bits "%bits%"
 if %errorlevel% neq 0 pause
 popd
