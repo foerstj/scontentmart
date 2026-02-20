ad2588d29e950ebce5fe7b9c98e9f0252f5c0488
Johannes Förstner <johannes@famfoerstner.de>
2026-02-20 14:50:57 +0100

    Generate merchant master templates as well

--- generate.bat
+++ generate.bat
@@ -4,4 +4,7 @@ set bits=%~dp0.
 pushd "%GasPy%"
 venv\Scripts\python -m jinja gaspy\armor-merchants.gas.jinja world\contentdb\templates\scontentmart\actors --for-all gaspy\armors.csv --bits "%bits%"
 if %errorlevel% neq 0 pause
+
+venv\Scripts\python -m jinja gaspy\shops.gas.jinja world\contentdb\templates\scontentmart\actors merchant.gas --for-all gaspy\shops.csv --bits "%bits%"
+if %errorlevel% neq 0 pause
 popd
