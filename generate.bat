c4b89042eb5e607866986ac6ebd838aa2a9fa788
Johannes Förstner <johannes@famfoerstner.de>
2026-02-21 14:18:44 +0100

    Rename armor->equipment step 1

--- generate.bat
+++ generate.bat
@@ -2,9 +2,9 @@
 set bits=%~dp0.
 
 pushd "%GasPy%"
-venv\Scripts\python -m jinja gaspy\armor-merchants.gas.jinja world\contentdb\templates\scontentmart\actors contents.gas --for-all gaspy\armors.csv --value v=v --bits "%bits%"
+venv\Scripts\python -m jinja gaspy\contents.gas.jinja world\contentdb\templates\scontentmart\actors contents.gas --for-all gaspy\equipments.csv --value v=v --bits "%bits%"
 if %errorlevel% neq 0 pause
-venv\Scripts\python -m jinja gaspy\armor-merchants.gas.jinja world\contentdb\templates\scontentmart\actors dsx-contents.gas --for-all gaspy\armors.csv --value v=loa --bits "%bits%"
+venv\Scripts\python -m jinja gaspy\contents.gas.jinja world\contentdb\templates\scontentmart\actors dsx-contents.gas --for-all gaspy\equipments.csv --value v=loa --bits "%bits%"
 if %errorlevel% neq 0 pause
 
 venv\Scripts\python -m jinja gaspy\shops.gas.jinja world\contentdb\templates\scontentmart\actors merchant.gas --for-all gaspy\shops.csv --value v=v --bits "%bits%"
