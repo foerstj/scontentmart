3fc977fabfb4ac3b84329e7755d3d351d64cca50
Johannes Förstner <johannes@famfoerstner.de>
2026-02-21 09:44:10 +0100

    Split generate into vanilla & dsx

--- generate.bat
+++ generate.bat
@@ -2,11 +2,13 @@
 set bits=%~dp0.
 
 pushd "%GasPy%"
-venv\Scripts\python -m jinja gaspy\armor-merchants.gas.jinja world\contentdb\templates\scontentmart\actors --for-all gaspy\armors.csv --bits "%bits%"
+venv\Scripts\python -m jinja gaspy\armor-merchants.gas.jinja world\contentdb\templates\scontentmart\actors contents.gas --for-all gaspy\armors.csv --value v=v --bits "%bits%"
+if %errorlevel% neq 0 pause
+venv\Scripts\python -m jinja gaspy\armor-merchants.gas.jinja world\contentdb\templates\scontentmart\actors dsx-contents.gas --for-all gaspy\armors.csv --value v=loa --bits "%bits%"
 if %errorlevel% neq 0 pause
 
 venv\Scripts\python -m jinja gaspy\shops.gas.jinja world\contentdb\templates\scontentmart\actors merchant.gas --for-all gaspy\shops.csv --value v=v --bits "%bits%"
 if %errorlevel% neq 0 pause
-venv\Scripts\python -m jinja gaspy\shops.gas.jinja world\contentdb\templates\scontentmart\actors dsx_merchant.gas --for-all gaspy\shops.csv --value v=loa --bits "%bits%"
+venv\Scripts\python -m jinja gaspy\shops.gas.jinja world\contentdb\templates\scontentmart\actors dsx-merchant.gas --for-all gaspy\shops.csv --value v=loa --bits "%bits%"
 if %errorlevel% neq 0 pause
 popd
