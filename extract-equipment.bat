2af88cbc4f6db89d986faf510b3d86f8f560fe0b
Johannes Förstner <johannes@famfoerstner.de>
2026-05-25 13:01:01 +0200

    Adapt to new gaspy script

--- extract-equipment.bat
+++ extract-equipment.bat
@@ -2,6 +2,6 @@
 set bits=%~dp0.
 
 pushd "%GasPy%"
-venv\Scripts\python -m printouts.equipment --bits "%bits%" --out "%bits%\gaspy"
+venv\Scripts\python -m printouts.scontentmart --bits "%bits%" --out "%bits%\gaspy"
 if %errorlevel% neq 0 pause
 popd
